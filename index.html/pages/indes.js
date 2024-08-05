b802d6b5c7e01440381f27f7c946d28b9c9d4ec3
quylala <84837550+quylala@users.noreply.github.com>
2024-08-06 06:16:29 +0700

    Update indes.js
    Signed-off-by: quylala <84837550+quylala@users.noreply.github.com>

--- index.html/pages/indes.js
+++ index.html/pages/indes.js
@@ -1,1 +1,8 @@
+export async function getStaticProps({ preview = false }) {
+  const allPosts = await getAllPostsForHome(preview)
 
+  return {
+    props: { allPosts, preview },
+    revalidate: 10,
+  }
+}
